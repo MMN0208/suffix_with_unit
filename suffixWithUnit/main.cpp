0268753606c405b99ace146d100ec68c887258a3
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-11-05 15:10:00 +0700

    k lol

--- suffixWithUnit/main.cpp
+++ suffixWithUnit/main.cpp
@@ -4,17 +4,41 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    int sel;
+    do {
+        cout << "Input the way you want to run the test:\n0: console input\n1: ./test/test.txt\nYour input: ";
+        cin >> sel;
+    } while(sel < 0 || sel > 1);
     string fileName = "./test/test.txt";
     ifstream myFile(fileName);
     ofstream resultFile("./result/result.txt");
-    if(myFile.is_open()) {
-        double unit = 0.0; // a variable to retrieve values from myFile
-        while(myFile >> unit) {
-            resultFile << unit << " = " << suffixWithUnit(unit) << endl;
-        }
+    switch(sel) {
+        case 0:
+            int n;
+            do {
+                cout << "Input the number of tests you want to run: ";
+                cin >> n;
+            } while(n < 1);
+            cout << "Input your test(s) here:\n";
+            double test;
+            for(int i = 0; i < n; i++) {
+                cin >> test;
+                resultFile << test << " = " << suffixWithUnit(test) << endl;
+                cout << test << " = " << suffixWithUnit(test) << endl;
+            }
+            break;
+        case 1:
+            if(myFile.is_open()) {
+                double unit = 0.0; // a variable to retrieve values from myFile
+                while(myFile >> unit) {
+                    resultFile << unit << " = " << suffixWithUnit(unit) << endl;
+                    cout << unit << " = " << suffixWithUnit(unit) << endl;
+                }
+            }
+            else return 1;
+            myFile.close();
+            resultFile.close();
+            break;
     }
-    else return 1;
-    myFile.close();
-    resultFile.close();
     return 0;
 }
